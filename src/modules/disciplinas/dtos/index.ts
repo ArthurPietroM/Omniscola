2a98008265ca8d5f912dd1c824bc4b6ee25953a7
ArthurPietroM <arthurpietro.mc07@gmail.com>
2026-07-26 08:34:43 -0300

    fix: adiciona DisciplinaResponseDTO ao dtos de disciplinas

--- src/modules/disciplinas/dtos/index.ts
+++ src/modules/disciplinas/dtos/index.ts
@@ -1,1 +1,18 @@
-export * from '@/shared/types';
+export interface CreateDisciplinaDTO {
+  nome: string;
+  codigo: string;
+  institutionId: string;
+}
+
+export interface UpdateDisciplinaDTO {
+  id: string;
+  nome?: string;
+  codigo?: string;
+}
+
+export interface DisciplinaResponseDTO {
+  id: string;
+  nome: string;
+  codigo: string;
+  institutionId: string;
+}
